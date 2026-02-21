bf9b09fea57c15e8d26cc13d182d79947e7a5a82
faryap <faryap1511@gmail.com>
2026-02-21 09:40:27 +0700

    membuat directory file database dan pdf

--- src/import.py
+++ src/import.py
@@ -3,3 +3,6 @@ import sqlite3
 from datetime import datetime
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
+
+DB_FILE = "kas_takmir.db"
+FILE_PDF = "laporan_kas_takmir.pdf"
