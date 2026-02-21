b10b8a804f86f15baac8b13c1be7026142cc4cd8
faryap <faryap1511@gmail.com>
2026-02-21 09:41:36 +0700

    membuat format dan input Rupiah

--- src/import.py
+++ src/import.py
@@ -6,3 +6,18 @@ from reportlab.pdfgen import canvas
 
 DB_FILE = "kas_takmir.db"
 FILE_PDF = "laporan_kas_takmir.pdf"
+
+# ===============================
+# FORMAT & INPUT RUPIAH
+# ===============================
+def format_rupiah(angka):   
+    return f"Rp {angka:,}".replace(",", ".")
+
+def input_rupiah(prompt):
+    while True:
+        try:
+            nilai = input(prompt)
+            nilai_bersih = nilai.replace(".", "").replace(",", "")
+            return int(nilai_bersih)
+        except ValueError:
+            print("❌ Masukkan angka yang benar! Contoh: 100.000")
