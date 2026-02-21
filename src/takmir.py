87303c0cd2265eded77df3a37258b76aee0303fa
Muhammad Furqon Atthoriq <muhammadfurqonatthoriq@smkitihsanulfikri.sch.id>
2026-02-21 09:47:58 +0700

    update file takmir

--- src/takmir.py
+++ src/takmir.py
@@ -7,3 +7,11 @@ def export_pdf():
     cur.execute("SELECT tanggal, jenis, jumlah, keterangan, petugas FROM kas")
     rows = cur.fetchall()
     conn.close()
+
+if not rows:
+        print("❌ Tidak ada data untuk diexport.")
+        return
+    
+    c = canvas.Canvas(FILE_PDF, pagesize=A4)
+    width, height = A4
+    y = height - 50
